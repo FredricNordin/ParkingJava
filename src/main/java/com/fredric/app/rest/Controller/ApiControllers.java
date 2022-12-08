f681751481378ebe5adbbd3cff96c9c66f413658
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-12-08 10:33:43 +0100

    Revert "add /api to all routes"
    This reverts commit bf006abb41b4d2f84b71a439b9abf4141c7a5230.

--- src/main/java/com/fredric/app/rest/Controller/ApiControllers.java
+++ src/main/java/com/fredric/app/rest/Controller/ApiControllers.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fredric.app.rest.Entities.Car;
@@ -22,6 +23,7 @@ import com.fredric.app.rest.Repo.ParkingSpotsRepo;
 import com.fredric.app.rest.Repo.PersonRepo;
 
 @RestController
+@RequestMapping("/api")
 public class ApiControllers {
     @Autowired PersonRepo personRepo;
     @Autowired CarRepo carRepo;
